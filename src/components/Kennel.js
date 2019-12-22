5a8b4fa1679acd80ecd3bd2a8479cc21870a83ca
Josh Webb <joshwebb42@gmail.com>
2019-12-21 22:02:32 -0600

    style

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import AnimalCard from './animal/AnimalCard'
 import './Kennel.css'
 
 class Kennel extends Component {
     render() {
         return (
+        <div>
             <div>
                 <h2>Students Kennels <br />
                     <small>Loving care when you're not there.</small>
@@ -13,6 +15,12 @@ class Kennel extends Component {
                     <br />500 Pyppy Way
                 </address>
             </div>
+            <div>
+                <AnimalCard />
+                <AnimalCard />
+                <AnimalCard />
+            </div>
+        </div>
         );
     }
 }
