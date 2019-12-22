494fdea2d90cea7447cc88c62dd541bf0787342b
Josh Webb <joshwebb42@gmail.com>
2019-12-21 20:29:11 -0600

    Kennel.js

---
+++ src/components/Kennel.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './Kennel.css'
+
+class Kennel extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Students Kennels <br />
+                    <small>Loving care when you're not there.</small>
+                </h2>
+                <address>
+                    Visit Us at the Nashville North Location
+                    <br />500 Pyppy Way
+                </address>
+            </div>
+        );
+    }
+}
+
+export default Kennel
