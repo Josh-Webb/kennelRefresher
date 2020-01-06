5853686edf50909e6ab888ae232272ef49b57708
Josh Webb <joshwebb42@gmail.com>
2020-01-06 00:12:02 -0600

    webpack

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -9,8 +9,8 @@ class AnimalCard extends Component {
           <picture>
             <img src={require('./dog.svg')} alt="My Dog" />
           </picture>
-          <h3>Name: <span className="card-petname">{this.props.animal.name}</span></h3>
-          <p>Breed: {this.props.animal.breed}</p>
+          <h3>Animal Name: <span className="card-petname">{this.props.animal.name}</span></h3>
+          <p>Animal Breed: {this.props.animal.breed}</p>
           <Link to={`/animals/${this.props.animal.id}`}><button>Details</button></Link>
           <button type="button"
           onClick={() => {this.props.history.push(`/animals/${this.props.animal.id}/edit`)}}>Edit</button>
